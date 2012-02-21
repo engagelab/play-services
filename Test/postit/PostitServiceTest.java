6149db133042da458c160bf34eec571b62240f1b
fahid lone <fahied@gmail.com>
2012-02-21 12:46:37 +0100

    UPDATE rest method works

--- Test/postit/PostitServiceTest.java
+++ Test/postit/PostitServiceTest.java
@@ -21,15 +21,22 @@ public class PostitServiceTest extends FunctionalTest {
 	
 	@Test
 	public void testPost() {
-        Response response = POST("/postit/","application/json","{ \"user\": \"jeremyt\", \"sceneId\": \"simulation1\" }"); 
+        Response response = POST("/postit/","application/json","{ \"user\": \"tony\", \"sceneId\": \"4\", \"content\": \"this is an update\", \"ypos\":\"304\", \"xpos\":\"338\" }"); 
 	    assertIsOk(response);
 	}
 	 
 	
-	@Test
+	@Ignore
 	public void testGet() {
 	       Response response = GET("/postit/");
 	       assertIsOk(response);
+	}
+	
+	//{"sceneId"=>nil, "_id"=>"4f4376d69c95054f3a000001", "ypos"=>"304", "xpos"=>"338", "content"=>"yoyo new text", "user"=>nil}
+	@Test
+	public void testUpdate() {
+		Response response = PUT("/postit/","application/json","{ \"id\": \"1\", \"user\": \"tony\", \"sceneId\": \"4\", \"content\": \"this is an update\", \"ypos\":\"304\", \"xpos\":\"338\" }"); 
+	    assertIsOk(response);
 	}
 
 }
--- app/controllers/Postits.java
+++ app/controllers/Postits.java
@@ -7,6 +7,7 @@ import models.Postit;
 
 import org.apache.commons.io.IOUtils;
 
+import play.db.jpa.JPABase;
 import play.mvc.Controller;
 
 import com.google.gson.Gson;
@@ -16,11 +17,12 @@ public class Postits extends Controller {
     public static void index() {
         render();
     }
-    
+   
+   //{user: "jeremyt", sceneId: "simulation1" }
    public static void create() throws IOException {
     	String json = IOUtils.toString(request.body);
-    	Postit temp = new Gson().fromJson(json, Postit.class);
-    	Postit p = new Postit(temp.sceneId, temp.user,"0", "0" );
+    	Postit tmpPostit = new Gson().fromJson(json, Postit.class);
+    	Postit p = new Postit(tmpPostit.sceneId, tmpPostit.user,"0", "0" );
     	p.save();
     	renderJSON(p);
     }
@@ -29,10 +31,34 @@ public class Postits extends Controller {
     
     public static void all() {
     	//TODO: need to send user and scene as parameters for the search
-    
     	List<Postit> findAll = Postit.findAll();
     	renderJSON(findAll);
     }
     
+    
+    public static void update() throws IOException {
+    	String json = IOUtils.toString(request.body);
+    	Postit tmpPostit = new Gson().fromJson(json, Postit.class);
+    	Postit foundPostit = Postit.findById(tmpPostit.id);
+    	
+    	//Postick newPostick = Postick.find("select distinct p from Postick p where p.randomId=?", randomId).first() ;
+    	
+    	foundPostit.sceneId = tmpPostit.sceneId;
+    	foundPostit.content = tmpPostit.content;
+    	foundPostit.user = tmpPostit.user;
+    	foundPostit.xpos = tmpPostit.xpos;
+    	foundPostit.ypos = tmpPostit.ypos;
+    	foundPostit.save();
+    	
+    	renderJSON(foundPostit);
+    }
+
+    /*
+     * Delete a Specific Postit using Unique ID
+     * */
+    public static void delete() {
+    	String uId = params.get("uId");
+		Postit.delete("from Postit p where p.uId=?", uId);
+    }
 
 }
