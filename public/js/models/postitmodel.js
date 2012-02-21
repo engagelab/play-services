14af57c9c75c854c87e8694e9e7ce82b43392271
jt151077 <jt151077@gmail.com>
2012-02-21 14:49:03 +0100

    remove unessecary object definition

--- public/js/models/postitmodel.js
+++ public/js/models/postitmodel.js
@@ -1,11 +1,4 @@
-window.Postit = Backbone.Model.extend({
-	 urlRoot:"../postits",
-    defaults:{
-        "id":null,
-        "scene":"",
-        "owner":""
-    }
-});
+window.Postit = Backbone.Model.extend({});
 
 window.PostitCollection = Backbone.Collection.extend({
     model:Postit,
