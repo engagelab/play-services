b11cf5ad749c84f0ec6a8421651e1110b83d7f2a
fahid lone <fahied@gmail.com>
2012-02-21 13:22:16 +0100

    added a overloaded constructor in Postit

--- app/models/Postit.java
+++ app/models/Postit.java
@@ -10,18 +10,26 @@ import java.util.*;
 public class Postit extends Model {
 	
 	//{"scene":"","_id":"4f3e3bd09c95052c17000004","ypos":"","xpos":"","content":"","user":""}
-	
-	public String sceneId;
-	public String ypos;
-	public String xpos;
-	public String content;
 	public String user;
+	public String sceneId;
+	public String content;
+	public float xpos;
+	public float ypos;
 	
-	public Postit(String sceneId, String ypos, String xpos, String user) {
+	public Postit(String user, String sceneId) {
+		this.user = user;
 		this.sceneId = sceneId;
+		this.content = "";
+		this.ypos = 0.0f;
+		this.xpos = 0.0f;
+	}
+	
+	public Postit(String user, String sceneId,String content, float ypos, float xpos) {
+		this.user = user;
+		this.sceneId = sceneId;
+		this.content = content;
 		this.ypos = ypos;
 		this.xpos = xpos;
-		this.user = user;
 	}
     
 }
