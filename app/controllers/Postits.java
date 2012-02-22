874bee373e77bc88f75225ea8f674f3067b9a2b1
fahid lone <fahied@gmail.com>
2012-02-22 12:25:03 +0100

    updated postit

--- app/controllers/Postits.java
+++ app/controllers/Postits.java
@@ -1,16 +1,24 @@
 package controllers;
 
 import java.io.IOException;
+
 import java.util.List;
 
-import models.Postit;
+import javax.persistence.Query;
+
+import models.*;
+
 
 import org.apache.commons.io.IOUtils;
 
+import play.db.jpa.GenericModel.JPAQuery;
+import play.db.jpa.JPA;
 import play.db.jpa.JPABase;
 import play.mvc.Controller;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 public class Postits extends Controller {
 
@@ -21,32 +29,37 @@ public class Postits extends Controller {
    //{user: "jeremyt", sceneId: "simulation1" }
    public static void create() throws IOException {
     	String json = IOUtils.toString(request.body);
+
     	Postit tmpPostit = new Gson().fromJson(json, Postit.class);
-    	Postit p = new Postit(tmpPostit.sceneId, tmpPostit.user);
+    	Postit p = new Postit(tmpPostit.user, tmpPostit.sceneId);
     	p.save();
     	renderJSON(p);
     }
-    
+    /*
+     * Show All postit on specific scene and specific user
+     * */
     public static void all() {
-    	//TODO: need to send user and scene as parameters for the search
-    	List<Postit> findAll = Postit.findAll();
-    	renderJSON(findAll);
+    	//TODO get user and sceneId parameters
+    	//JPQL query to find all postits from Postit Table specific to the user and the scene
+    	List<Postit> postits = Postit.find("select p from Postit p where p.user =? and p.sceneId=?","fahied","Simmulation 1").fetch();
+    	//JPA Function return all postits regardless of user and scene
+    	//List<Postit> findAll = Postit.findAll();
+        //render 404 page if the object does not found
+    	notFoundIfNull(postits);
+        renderJSON(postits);
     }
     
     public static void update() throws IOException {
     	String json = IOUtils.toString(request.body);
     	Postit tmpPostit = new Gson().fromJson(json, Postit.class);
-    	Postit foundPostit = Postit.findById(tmpPostit.id);
-    	
-    	//Postick newPostick = Postick.find("select distinct p from Postick p where p.randomId=?", randomId).first() ;
-    	
+        Postit foundPostit = Postit.find("select distinct p from Postit p where p.uid=?", tmpPostit.uid).first() ;
+        notFoundIfNull(foundPostit);
     	foundPostit.sceneId = tmpPostit.sceneId;
     	foundPostit.content = tmpPostit.content;
     	foundPostit.user = tmpPostit.user;
     	foundPostit.xpos = tmpPostit.xpos;
     	foundPostit.ypos = tmpPostit.ypos;
     	foundPostit.save();
-    	
     	renderJSON(foundPostit);
     }
 
--- app/models/Postit.java
+++ app/models/Postit.java
@@ -1,7 +1,9 @@
 package models;
 
 import play.*;
+import play.data.validation.*;
 import play.db.jpa.*;
+import play.libs.Codec;
 
 import javax.persistence.*;
 import java.util.*;
@@ -10,11 +12,25 @@ import java.util.*;
 public class Postit extends Model {
 	
 	//{"scene":"","_id":"4f3e3bd09c95052c17000004","ypos":"","xpos":"","content":"","user":""}
-	public String user;
-	public String sceneId;
-	public String content;
+	@Required
+	 public String user;
+
+    @Required
+    public String sceneId;
+    
+//    @ManyToOne
+//    @Required
+//    public Scene scene;
+
+	@Lob
+    @Required
+    @MaxSize(10000)
+    public String content;
+	
 	public float xpos;
 	public float ypos;
+	public Date postedAt;
+	public String uid;
 	
 	public Postit(String user, String sceneId) {
 		this.user = user;
@@ -22,7 +38,9 @@ public class Postit extends Model {
 		this.content = "";
 		this.ypos = 0.0f;
 		this.xpos = 0.0f;
-	}
+		this.postedAt = new Date();
+		this.uid = Codec.UUID();
+		}
 	
 	public Postit(String user, String sceneId,String content, float ypos, float xpos) {
 		this.user = user;
@@ -30,6 +48,7 @@ public class Postit extends Model {
 		this.content = content;
 		this.ypos = ypos;
 		this.xpos = xpos;
+		this.postedAt = new Date();
 	}
     
 }
