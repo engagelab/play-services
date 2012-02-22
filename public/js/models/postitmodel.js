7c59746e4e09d93bc5de5a861a2cf8878dfb1314
jt151077 <jt151077@gmail.com>
2012-02-22 11:48:05 +0100

    backbone/play working instance, but only READ restful

--- public/js/models/postitmodel.js
+++ public/js/models/postitmodel.js
@@ -2,5 +2,5 @@ window.Postit = Backbone.Model.extend({});
 
 window.PostitCollection = Backbone.Collection.extend({
     model:Postit,
-    url:"http://localhost:9000/postit/"
+    url:"/postit/"
 });
