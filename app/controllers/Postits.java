c0836c23f9f04edf1b4ce1e90c5440f9d7adbbd8
fahid lone <fahied@gmail.com>
2012-02-23 12:35:16 +0100

    routes file updated

--- app/controllers/Postits.java
+++ app/controllers/Postits.java
@@ -33,26 +33,40 @@ public class Postits extends Controller {
     	Postit tmpPostit = new Gson().fromJson(json, Postit.class);
     	Postit p = new Postit(tmpPostit.user, tmpPostit.sceneId);
     	p.save();
+    	// Create or Update the scene
+//    	Scene scene = Scene.find("select s from Scene s where s.user =?",tmpPostit.sceneId).first();
+//    	if (scene != null)
+//    		//do nothing
+//    		System.out.println("Scene already exist");
+//    		
+//    	else
+//    	{
+//    		Scene newScene = new Scene(tmpPostit.user, tmpPostit.sceneId);
+//    		newScene.save();
+//    	}
+    		
     	renderJSON(p);
     }
     /*
      * Show All postit on specific scene and specific user
      * */
-    public static void all() throws IOException {
-    	//if request is JSON
-    	//String json = IOUtils.toString(request.body);
-    	//Scene tmpScene = new Gson().fromJson(json, Scene.class);
-    	//notFoundIfNull(tmpScene);
-    	
+    public static void all() {
+    	//JPA Function return all postits regardless of user and scene
+    	List<Postit> findAll = Postit.findAll();
+
+        //render 404 page if the object does not found
+    	notFoundIfNull(findAll);
+        renderJSON(findAll);
+    }
+    
+    public static void byUserAndScene() {
     	//if request is parameters
     	String user = params.get("user");
     	String sceneId = params.get("sceneId");
-    	
-    	//TODO get user and sceneId parameters
+    
     	//JPQL query to find all postits from Postit Table specific to the user and the scene
     	List<Postit> postits = Postit.find("select p from Postit p where p.user =? and p.sceneId=?",user,sceneId).fetch();
-    	//JPA Function return all postits regardless of user and scene
-    	//List<Postit> findAll = Postit.findAll();
+    
         //render 404 page if the object does not found
     	notFoundIfNull(postits);
         renderJSON(postits);
--- app/controllers/Scenes.java
+++ app/controllers/Scenes.java
@@ -3,18 +3,24 @@ package controllers;
 import java.util.List;
 
 import models.*;
+import play.db.jpa.GenericModel.JPAQuery;
+import play.db.jpa.JPABase;
 import play.mvc.Controller;
 
 public class Scenes extends Controller {
 	
     public static void all() {
-    	//TODO get user and sceneId parameters
-    	//JPQL query to find all Scenes from scene Table specific to the user and the scene
-    	List<Scene> scenes = Scene.find("select p from Scene p where p.user =?","fahied").fetch();
-    	//JPA Function return all scenes regardless of user and scene
-    	//List<Postit> findAll = Postit.findAll();
-        //render 404 page if the object does not found
-    	notFoundIfNull(scenes);
-        renderJSON(scenes);
+    	List<Scene> findAll = Scene.findAll();
+    	//render 404 page if the object does not found
+    	//notFoundIfNull(scenes);
+        renderJSON(findAll);
     }
+    
+    //GET ALL scenes by a specific user
+    public static void byUser(String userId) {
+    	//JPQL query to find all Scenes from scene Table specific to the user and the scene
+    	List<Scene> scenes = Scene.find("select p from Scene p where p.user =?",userId).fetch();
+    	renderJSON(scenes);
+    }
+    
 }
