d87eac850975daa390c522025aa63fc43ac26b1e
jt151077 <jt151077@gmail.com>
2012-02-23 12:34:49 +0100

    cleaned main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,77 +1,96 @@
-Backbone.View.prototype.close = function () {
-    console.log('Closing view ' + this);
-    if (this.beforeClose) {
-        this.beforeClose();
-    }
-    this.remove();
-    this.unbind();
+Backbone.View.prototype.close = function() {
+	console.log('Closing view ' + this);
+	if(this.beforeClose) {
+		this.beforeClose();
+	}
+	this.remove();
+	this.unbind();
 };
-
 var AppRouter = Backbone.Router.extend({
 
-	initialize:function () {
-        $('#header').html(new HeaderView().render().el);
-        var appSelf = this;
-    },
-
-    routes:{
-        "":"list",
-        "/simulations" : "viewSimulations"
-    },
+	initialize : function() {
+		$('#header').html(new HeaderView().render().el);
+	},
+	routes : {
+		"" : "list",
+		"/simulations" : "viewSimulations"
+	},
 
 	//executed when root path called
-    list:function () {
-    	app.initPitBoard();
-    	app.showView('#scenes', new ScenePickerView(), 'menu');
-    	this.before();
-    },
-    
-    //create new postit
-    newPostit:function () {
-       app.postitList.create({wait : true, user: "fahied", sceneId: app.currentViewId });
-    },
-    
-    //switch to simulations view
-    viewSimulations:function () {
-    	app.initPitBoard();
-    	app.simulation = new SimulationsView();
-        app.showView('#scenes', app.simulation, 'simu1');
-        this.before();
-    },
-
+	list : function() {
+		app.initPitBoard();
+		app.showView('#scenes', new ScenePickerView(), 'menu');
+	},
+	//create new postit
+	newPostit : function() {
+		app.postitList.create({
+			wait : true,
+			user : "fahied",
+			sceneId : app.currentViewId
+		});
+	},
+	//switch to simulations view
+	viewSimulations : function() {
+		app.initPitBoard();
+		app.simulation = new SimulationsView();
+		app.showView('#scenes', app.simulation, 'simu1');
+	},
 	//view switcher function
-    showView:function (selector, view, viewid) {
-        if (this.currentView) {
-            this.currentView.close();
-        }
-        $(selector).html(view.render().el);
-        this.currentView = view;
-        this.currentViewId = viewid;
-        return view;
-    },
-    
-    initPitBoard:function() {
-    	$('#board').html('');
-        app.postitList = null;
-    },
-
+	showView : function(selector, view, viewid) {
+		if(this.currentView) {
+			this.currentView.close();
+		}
+		$(selector).html(view.render().el);
+		this.currentView = view;
+		this.currentViewId = viewid;
+		//reload the postits for the scene
+		this.retrievePostIts();
+		return view;
+	},
+	initPitBoard : function() {
+		$('#board').html('');
+		app.postitList = null;
+	},
+	retrievePostIts : function() {
+		this.postitList = new PostitCollection();
+		this.postitList.fetch({
+			data : {
+				user : "fahied",
+				sceneId : app.currentViewId
+			},
+			success : function() {
+				$('#board').html(new PostitListView({
+					model : app.postitList
+				}).render().el);
+			}
+		});
+	},
 	//preloader of collections
-    before:function (callback) {
-        if (this.postitList) {
-            if (callback) callback();
-        } else {
-            this.postitList = new PostitCollection();
-            this.postitList.fetch({data: { user: "fahied", sceneId: app.currentViewId }, success:function () {
-                $('#board').html(new PostitListView({model:app.postitList}).render().el);
-                if (callback) callback();
-            }});
-        }
-    }
-
+	/*before : function(callback) {
+		if(this.postitList) {
+			if(callback)
+				callback();
+		} else {
+			this.postitList = new PostitCollection();
+			this.postitList.fetch({
+				data : {
+					user : "fahied",
+					sceneId : app.currentViewId
+				},
+				success : function() {
+					$('#board').html(new PostitListView({
+						model : app.postitList
+					}).render().el);
+					if(callback)
+						callback();
+				}
+			});
+		}
+	}*/
 });
 
 //loading html templates
-tpl.loadTemplates(['header', 'postit-item', 'scene-picker', 'simulations'], function () {
-    app = new AppRouter();
-    Backbone.history.start();
+tpl.loadTemplates(['header', 'postit-item', 'scene-picker', 'simulations'], function() {
+	app = new AppRouter();
+	Backbone.history.start();
 });
--- public/js/views/simulations.js
+++ public/js/views/simulations.js
@@ -13,16 +13,35 @@ window.SimulationsView = Backbone.View.extend({
     },
 
     events:{
-        "click #sim-separator":"handleBarClicked"
+        "click #sim-separator":"handleBarClicked",
+        "click #sim-tab1":"showTab1",
+        "click #sim-tab2":"showTab2"
     },
 
     handleBarClicked:function (event) {
         event.preventDefault();
-        if($('#main').hasClass('use-sidebar')) {
-        	$('#main').removeClass('use-sidebar');
+        if($('#div-tab1').hasClass('use-sidebar')) {
+        	$('#div-tab1').removeClass('use-sidebar');
 		}
 		else {
-			$('#main').addClass('use-sidebar');
+			$('#div-tab1').addClass('use-sidebar');
 		}
+    },
+    
+    showTab1:function(event) {
+    	event.preventDefault();
+    	$('#div-tab2').hide();
+    	$('#sim-tab2').removeClass('sim-active');
+    	$('#sim-tab1').addClass('sim-active');
+    	$('#div-tab1').show();
+    	
+    },
+    
+    showTab2:function(event) {
+    	event.preventDefault();
+    	$('#div-tab1').hide();
+    	$('#sim-tab1').removeClass('sim-active');
+    	$('#sim-tab2').addClass('sim-active');
+    	$('#div-tab2').show();
     }
 });
