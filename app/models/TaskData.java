7367dd221148bd69a587f9a087b09c1f91c3fc84
fahid lone <fahied@gmail.com>
2012-03-09 19:48:51 +0100

    ignore

--- app/models/TaskData.java
+++ app/models/TaskData.java
@@ -1,12 +1,19 @@
 package models;
 
-import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
+import java.util.Date;
+import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.PrePersist;
+
+import play.data.validation.MaxSize;
 import play.db.jpa.Model;
 
 @Entity
-public class TaskData extends Model {
+public class TaskData extends Model{
+	
 	@ManyToOne
     public MyGroup myGroup;
 	
@@ -15,25 +22,40 @@ public class TaskData extends Model {
 	 public Project project;
 	
 	@ManyToOne
-	 public Task task;
+	public Task task;
 	
 	public Long run_id;
 	
-	public String data;
+	public String taskdata;
+
+
+	@PrePersist
+	public void prePersist(){
+		taskdata = "1000";
+	}
 	
-	public TaskData(Project project, Long run_id, MyGroup myGroup, Task task){
+	public TaskData(Project project, Long run_id, MyGroup myGroup, Task task) {
 		this.project = project;
 		this.run_id = run_id;
 		this.myGroup = myGroup;
 		this.task = task;
-		this.data = "";
-	}
-	
-	public TaskData(Project project, Long run_id, MyGroup myGroup, Task task, String data){
-		this.project = project;
-		this.run_id = run_id;
-		this.myGroup = myGroup;
-		this.task = task;
-		this.data = data;
-	}
+		}
 }
+
+//	public static TaskData findByTaskAndGroup(Task task, MyGroup group) {
+//		TaskData data = TaskData.find("byTaskandMyGroup",task,group);
+//		return data;
+//	}
+
+	
+//	public static void deleteComment(Long id) {
+//		Comment.delete("from Comment c where c.id=?", id);
+//	}
+//
+//	
+//	public static Comment updateComment(Long id, String title, String message) {
+//		Comment myComment = Comment.findById(id);
+//		return myComment;
+//	}
+
+
