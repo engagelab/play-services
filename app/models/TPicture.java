7a9d46df1e608dbf2800c88c6a9ae37d5c29dd7f
Fahied <fahied@gmail.com>
2012-04-02 18:25:49 +0200

    simple file uploader added
    /upload

---
+++ app/models/TPicture.java
@@ -0,0 +1,51 @@
+package models;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.PrePersist;
+
+import play.db.jpa.Model;
+
+@Entity
+public class TPicture extends Model{
+
+	public String FILE_PATH;
+	public String filename;
+	
+	@ManyToOne
+    public MyGroup myGroup;
+	
+	@ManyToOne
+    public Task task;
+	
+	public Date postedAt;
+
+	public int wxpos;		//x position of youtube box on web
+	public int wypos;		//y position of youtube box on web
+	
+	@PrePersist
+	public void prePersist(){
+		postedAt = new Date();
+		FILE_PATH = "./public/upload/";
+		wxpos = 0;
+		//startup position of you-tube box on web
+		wypos = 200;
+	}
+	
+	public TPicture(String filename, MyGroup myGroup, Task task) {
+		this.filename = filename;
+		this.myGroup = myGroup;
+		this.task = task;
+	}
+	
+	public TPicture(String filename, MyGroup myGroup, Task task, int wxpos, int wypos) {
+		this.filename = filename;
+		this.myGroup = myGroup;
+		this.task = task;
+		this.wxpos = wxpos;
+		this.wypos = wypos;
+	}
+
+}
